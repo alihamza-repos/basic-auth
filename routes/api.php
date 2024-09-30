c02613d3874ab2e5f953583702d10ec946f1f980
alihamza-repos <alihamza.brandixsoft@gmail.com>
2024-09-30 20:58:12 +0500

    changed post model and api to show post data

--- routes/api.php
+++ routes/api.php
@@ -6,7 +6,7 @@ use Illuminate\Support\Facades\Route;
 // Define the route for the API
 Route::get('/posts', [PostController::class, 'index']); // Get all posts
 Route::post('/posts', [PostController::class, 'store']); // Create a new post
-Route::get('/posts/{id}', [PostController::class, 'show']); // Get a single post
-Route::put('/posts/{id}', [PostController::class, 'update']); // Update a post
-Route::delete('/posts/{id}', [PostController::class, 'destroy']); // Delete a post
+Route::get('/posts/{post}', [PostController::class, 'show']); // Get a single post
+Route::put('/posts/{post}', [PostController::class, 'update']); // Update a post
+Route::delete('/posts/{post}', [PostController::class, 'destroy']); // Delete a post
 
